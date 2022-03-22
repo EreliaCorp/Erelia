897213ae403b86da6046aa3e1c48ca17721a2827
Hyarius <Boissy.jeremy@yahoo.fr>
2022-03-22 20:06:11 +0100

    Resolve conflic

--- Erelia/srcs/Widget/Screen/erelia_loading_screen.cpp
+++ Erelia/srcs/Widget/Screen/erelia_loading_screen.cpp
@@ -8,7 +8,6 @@ void Loading_screen::_on_geometry_change()
 Loading_screen::Loading_screen(jgl::Widget* p_parent) : Abstract_screen(p_parent)
 {
 	button = new jgl::Button([&](jgl::Data_contener& p_param) {
-		jgl::cout << "Loading screen button" << jgl::endl;
 		}, this);
 	button->label().set_text("Go world");
 	button->activate();
